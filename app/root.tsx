9cd290d26da8b289791dfc77516f61b42fddb69a
havanoor <havanoor@gmail.com>
2025-03-09 12:57:27 +0530

    New 404 error page

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,21 @@
 import {
+  isRouteErrorResponse,
   json,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useRouteError,
 } from "@remix-run/react";
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 
 import "./tailwind.css";
 import NavBar from "../components/Navbar";
 import { getSession } from "./lib/helperFunctions";
+import { ArrowLeft, Home } from "lucide-react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   let user = await getSession(request);
@@ -44,4 +48,53 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className="flex flex-col items-center justify-center min-h-screen  px-4">
+          <div className="text-center max-w-md">
+            <h1 className="text-9xl font-bold text-gray-900 animate-pulse">
+              404
+            </h1>
+
+            <h2 className="mt-8 text-2xl font-semibold text-gray-800">
+              Page not found
+            </h2>
+            <p className="mt-4 text-gray-600">
+              Oops! The page you're looking for seems to have wandered off into
+              the digital wilderness.
+            </p>
+
+            <div className="mt-10 flex flex-col sm:flex-row items-center justify-center gap-4">
+              <Link
+                to="/"
+                className="flex items-center gap-2 px-6 py-3 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors"
+              >
+                <Home size={18} />
+                <span>Go Home</span>
+              </Link>
+
+              <button
+                onClick={() => window.history.back()}
+                className="flex items-center gap-2 px-6 py-3 border border-gray-300 rounded-lg hover:bg-gray-100 transition-colors"
+              >
+                <ArrowLeft size={18} />
+                <span>Go Back</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <Scripts />
+      </body>
+    </html>
+  );
 }
